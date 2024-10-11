479bab6f816b57eebec12e04d0988a95c923b227
Ameli <140230316+aameliig@users.noreply.github.com>
2024-10-11 10:20:53 +0300

    Delete 1.build_all_images directory

--- 1.build_all_images/namenode/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "`ls -A /hadoop/dfs/name`" == "" ]; then
-  # format hdfs if first start
-  $HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
-fi
-
-$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR namenode
